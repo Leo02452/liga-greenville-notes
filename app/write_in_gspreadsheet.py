6fb6c4eb46003b95e39f077844f6057744a2841b
Leo Araujo <leonardo_02452@hotmail.com>
2023-07-08 18:45:42 -0300

    feat: add function that write player data in a google spreadsheet

---
+++ app/write_in_gspreadsheet.py
@@ -0,0 +1,20 @@
+import gspread
+
+def write_players_notes_in_google_sheets(players_list, game_info):
+    gc = gspread.oauth()
+    spreadsheet = gc.open("23 - Argentina_Greenville")
+    worksheet = spreadsheet.worksheet("Data")
+    column_a = worksheet.col_values(1)
+    first_avaiable_row = len(column_a) + 1
+    data_to_add = []
+    for player in players_list:
+        data_to_add.append([ 
+            player["position"],
+            player["name"],
+            player["note"].replace('.', ','),
+            game_info['team'],
+            game_info['opponent'],
+            game_info['competition']
+        ])
+    cell_range = f'A{first_avaiable_row}:F{first_avaiable_row + len(data_to_add) - 1}'
+    worksheet.update(cell_range, data_to_add)
